30488a145d5dbcd671022e4920172ffd0f44a49e
Luke Grunau <lukegrunau@Lukes-MBP.fritz.box>
2022-12-14 15:57:19 +0100

    dwellir

--- src/utilities/subscanHost.ts
+++ src/utilities/subscanHost.ts
@@ -2,6 +2,7 @@ const subscanHosts: Record<string, string> = {
   'wss://peregrine.kilt.io/parachain-public-ws':
     'https://kilt-testnet.subscan.io',
   'wss://spiritnet.kilt.io': 'https://spiritnet.subscan.io',
+  'wss://kilt-rpc.dwellir.com': 'https://spiritnet.subscan.io',
 };
 
 export function getSubscanHost(): string {
